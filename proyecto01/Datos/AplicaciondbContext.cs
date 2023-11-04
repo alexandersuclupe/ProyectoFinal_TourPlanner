b3946c4c417dd4ad3110aa5f8405e6214c83d22a
alexandersuclupe <115763881+alexandersuclupe@users.noreply.github.com>
2023-11-04 11:48:49 -0500

    Agregar archivos de proyecto.

---
+++ proyecto01/Datos/AplicaciondbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyecto01.Models;
+
+namespace proyecto01.Datos
+{
+    public class AplicacionDbContext : DbContext
+    {
+        public AplicacionDbContext(DbContextOptions<AplicacionDbContext>options) : base(options)
+        {
+            
+        }
+        //Agergar el modelo categoria a nuestro DbContext
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
---
+++ proyecto01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyecto01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
